1a82d7d633df0d287d796671098cec1b36f58658
Safiull Alam <safiullalam9931@gmail.com>
2023-12-10 10:52:14 +0600

    duplicate row added and remove on click.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
-
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
 
 function Home() {
-  return <h1>Working with react router.</h1>;
+  const [tbodyCount, setTbodyCount] = useState(1);
+
+  const handleAddRow = () => {
+    setTbodyCount(prevCount => prevCount + 1);
+  };
+
+  const handleRemoveRow = () => {
+    if (tbodyCount > 1) {
+      setTbodyCount(prevCount => prevCount - 1);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleAddRow}>+</button>
+      <table>
+        {[...Array(tbodyCount)].map((_, index) => (
+          <tbody key={index}>
+            <tr>
+              <td><input type='text' /></td>
+              <td><input type='text' /></td>
+              <td><input type='text' /></td>
+              <td><input type='text' /></td>
+              <td><button onClick={handleRemoveRow}>-</button></td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
+    </div>
+  );
 }
 
-let root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home brand="col" />);
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<Home />);
